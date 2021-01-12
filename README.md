c939ec3c3a489fc2829b2ba93aea8ade604e8a94
Mohit Kumar <mkay6123@gmail.com>
2021-01-13 01:14:25 +0530

    Init App

---
+++ README.md
@@ -0,0 +1,4 @@
+# Roomie Finder
+
+Roomie Finder is the must have app for every college. Transition from school to college is not easy. Getting a suitabloe roommate in the hostel, helps adjust faster and in a better way.
+Each hosteller is supposed to fill a form, which includes some preferences of student and based on their filled prefe4rneces they are matched with most similar suitable roommate...
---
+++ hostel.py
@@ -0,0 +1,68 @@
+from flask import Flask, flash, redirect, render_template, request
+
+app = Flask(__name__)
+app.secret_key = 'P\x02_\xb0$.\x1c\xb4\x02:q\x14\x9d\xcf}\r'
+data = ""
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/form/", methods=["GET", "POST"])
+def form():
+    return render_template("form.html", title=form)
+
+
+@app.route("/responses", methods=["POST"])
+def responses():
+    if request.method == "POST":
+        file1 = open("entries.txt", "a")
+        global data
+        data = ""
+        data += request.form["roll_number"] + ","
+        print(
+            f"Data Requested for the Roll Number in the above form using the flask method id as above  {request.form['Name']}"
+        )
+        data += request.form["rad1"]
+        data += request.form["rad2"]
+        data += request.form["rad5"]
+        data += request.form["rad4"]
+        data += request.form["rad3"]
+        file1.write(data)
+        file1.write("\n")
+        file1.close()
+        return redirect("/answer")
+    else:
+        print("Error in Searching.. !\n Please Try Again..!\n")
+        return redirect("/")
+
+
+@app.route("/answer", methods=["GET", "POST"])
+def answer():
+    diff = 30000
+    file1 = open("entries.txt", "r")
+    lines = file1.readlines()
+    # print(lines)
+    a = int(lines[-1].split(",")[1])
+    # print(a)
+    l = lines[0]
+    for line in lines[0:-2]:
+        b = int(line.split(",")[1])
+        # print(b)
+        if abs(b - a) < diff:
+            if abs(b - a) != 0:
+                diff = abs(b - a)
+                l = line
+    rn = l.split(",")[0]
+    file1.close()
+    return render_template("answer.html", data=str(rn))
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title=about)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
